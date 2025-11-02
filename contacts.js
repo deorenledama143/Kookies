b906cc89eaa87839d6beca3c5a6077e1e0c50cdd
Ken Jorique <ken20042011@gmail.com>
2025-11-02 23:51:12 +0800

    contact

---
+++ contacts.js
@@ -0,0 +1,26 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const message = document.getElementById('message').value.trim();
+
+  if (name && email && message) {
+    alert(`Thank you, ${name}! Your message has been sent successfully.`);
+    this.reset();
+  } else {
+    alert("Please fill in all fields before submitting.");
+  }
+});
+
+ const backToTop = document.getElementById("backToTop");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+backToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
